804e4fe52e69e62492cc90da9351010df35cc488
Renata Romero <re_ro_me@hotmail.es>
2018-11-01 22:25:22 -0600

    Save button listener added

--- app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityItem.java
+++ app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityItem.java
@@ -2,11 +2,13 @@ package com.iteso.pdm18_scrollabletabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.iteso.pdm18_scrollabletabs.beans.ItemProduct;
 import com.iteso.pdm18_scrollabletabs.database.DataBaseHandler;
 import com.iteso.pdm18_scrollabletabs.beans.Category;
 import com.iteso.pdm18_scrollabletabs.beans.Store;
@@ -37,7 +39,7 @@ public class ActivityItem extends AppCompatActivity {
         CategoryControl categoryControl = new CategoryControl();
         StoreControl storeControl = new StoreControl();
 
-        ArrayList<Category> categories = categoryControl.getCategories(dh);
+        final ArrayList<Category> categories = categoryControl.getCategories(dh);
         ArrayList<Store> stores = storeControl.getStores(dh);
 
         ArrayList<String> namesCategories = new ArrayList<String>();
@@ -66,7 +68,26 @@ public class ActivityItem extends AppCompatActivity {
         tiendas.setAdapter(namesStoresAdapter);
         imagenes.setAdapter(namesImagesAdapter);
 
-    }
+        guardar.setOnClickListener(new View.OnClickListener(){
 
-    guardar.OnClickListener
+            @Override
+            public void onClick(View v) {
+
+                ItemProduct itemProduct = new ItemProduct();
+                Category category = new Category();
+                Store store = new Store();
+
+                category.setName(categorias.getSelectedItem().toString());
+                store.setName(tiendas.getSelectedItem().toString());
+
+                itemProduct.setCategory(category);
+                itemProduct.setStore(store);
+
+                itemProduct.setTitle(titulo.getText().toString());
+                itemProduct.setImage(imagenes.getSelectedItemPosition());
+
+            }
+        });
+
+    }
 }
